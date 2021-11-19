24537c3c853f4e851149fafeed9a15be2cb7657d
THEspager <danilagon@icloud.com>
2021-11-19 07:39:58 +0100

    test 0.0.1 /Life Survival

--- src/script.js
+++ src/script.js
@@ -6,8 +6,8 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const geometry = new THREE.CircelGeometry();
+const material = new THREE.MeshBasicMaterial( { color: 0xff0505 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
